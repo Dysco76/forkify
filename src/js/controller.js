e8f6efa941e0e7ece137418de4be8304afdb2007
Dmytro Shcherba <dmytrosc@wix.com>
2022-02-10 20:45:54 +0200

    refactored for DOM update algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -14,6 +14,9 @@ const controlRecipes = async function () {
 
     recipeView.renderSpinner();
 
+    // 0) Update results view to mark selected serach result
+    resultsView.update(model.getSearchResultsPage());
+
     // 1) Loading recipe
     await model.loadRecipe(id);
 
@@ -59,7 +62,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
